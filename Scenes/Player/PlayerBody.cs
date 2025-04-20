26444ba4b0025dc3d313188fcb037baab2fdd51d
Rodrigo Travessa <rodrigosigiliao@outlook.com>
2025-04-20 11:23:05 -0300

    basic player movement, Main menu UI

--- Scenes/Player/PlayerBody.cs
+++ Scenes/Player/PlayerBody.cs
@@ -1,19 +1,24 @@
 using Godot;
 using System;
 
-public partial class CharacterBody2d : CharacterBody2D
+public partial class PlayerBody : CharacterBody2D
 {
-	public const float Speed = 300.0f;
-	public const float JumpVelocity = -400.0f;
+	public float Speed = 300.0f;
 
 	public override void _PhysicsProcess(double delta)
 	{
+
 		Vector2 velocity = Velocity;
+		if(Input.IsActionPressed("move_dash")){
+			Speed = 600.0f;
+		} else{
+			Speed = 300.0f;
+		}
 
 		
 		// Get the input direction and handle the movement/deceleration.
 		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+		Vector2 direction = Input.GetVector("move_left", "move_right", "move_up", "move_down");
 		if (direction != Vector2.Zero)
 		{
 			velocity.X = direction.X * Speed;
